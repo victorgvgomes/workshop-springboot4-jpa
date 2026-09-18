c9f615e8a5cde17b80a76d8a61ba42dadec1df66
Victor Gomes <victorgvgomes@gmail.com>
2026-09-18 14:50:27 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +57,19 @@ public class UserResource {
 		
 			
 		return ResponseEntity.created(uri).body(obj);
+		//É feito desta maneira, por que usando 	
+		//return ResponseEntity.noContent().build(); o Status é 200, e não 201 Created.
+		
+
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User>update(@RequestBody User obj, @PathVariable Long id){
+	
+		obj = service.update(id, obj);
+		
+		
+			
+		return ResponseEntity.ok().body(obj);
 		//É feito desta maneira, por que usando 	
 		//return ResponseEntity.noContent().build(); o Status é 200, e não 201 Created.
 		
